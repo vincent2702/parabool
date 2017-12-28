0867855f07205a011fc995dd00ba9fd88e962fab
vincent2702 <vincent.schelwald@hotmail.com>
2017-12-28 23:44:25 +0100

    added streeplijst project

---
+++ streeplijst/streeplijst/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace streeplijst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Bier1 = new System.Windows.Forms.Button();
+            this.Bier2 = new System.Windows.Forms.Button();
+            this.Fris1 = new System.Windows.Forms.Button();
+            this.Fris2 = new System.Windows.Forms.Button();
+            this.Klaar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.name = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(308, 274);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(210, 113);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Wijn +";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Wijn_Click0);
+            // 
+            // button4
+            // 
+            this.button4.CausesValidation = false;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(308, 400);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(210, 95);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Wijn -";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Wijn_Click1);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button1.Location = new System.Drawing.Point(590, 274);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 113);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Snoep +";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Snoep_Click0);
+            // 
+            // button2
+            // 
+            this.button2.CausesValidation = false;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button2.Location = new System.Drawing.Point(590, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(210, 95);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Snoep -";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Snoep_Click1);
+            // 
+            // Bier1
+            // 
+            this.Bier1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bier1.Location = new System.Drawing.Point(308, 22);
+            this.Bier1.Name = "Bier1";
+            this.Bier1.Size = new System.Drawing.Size(210, 113);
+            this.Bier1.TabIndex = 0;
+            this.Bier1.Text = "Bier +";
+            this.Bier1.UseVisualStyleBackColor = true;
+            this.Bier1.Click += new System.EventHandler(this.Bier_Click0);
+            // 
+            // Bier2
+            // 
+            this.Bier2.CausesValidation = false;
+            this.Bier2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bier2.Location = new System.Drawing.Point(308, 151);
+            this.Bier2.Name = "Bier2";
+            this.Bier2.Size = new System.Drawing.Size(210, 95);
+            this.Bier2.TabIndex = 1;
+            this.Bier2.Text = "Bier -";
+            this.Bier2.UseVisualStyleBackColor = true;
+            this.Bier2.Click += new System.EventHandler(this.Bier_Click1);
+            // 
+            // Fris1
+            // 
+            this.Fris1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Fris1.Location = new System.Drawing.Point(590, 22);
+            this.Fris1.Name = "Fris1";
+            this.Fris1.Size = new System.Drawing.Size(210, 113);
+            this.Fris1.TabIndex = 0;
+            this.Fris1.Text = "Fris +";
+            this.Fris1.UseVisualStyleBackColor = true;
+            this.Fris1.Click += new System.EventHandler(this.Fris_Click0);
+            // 
+            // Fris2
+            // 
+            this.Fris2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Fris2.Location = new System.Drawing.Point(590, 151);
+            this.Fris2.Name = "Fris2";
+            this.Fris2.Size = new System.Drawing.Size(210, 95);
+            this.Fris2.TabIndex = 1;
+            this.Fris2.Text = "Fris -";
+            this.Fris2.UseVisualStyleBackColor = true;
+            this.Fris2.Click += new System.EventHandler(this.Fris_Click1);
+            // 
+            // Klaar
+            // 
+            this.Klaar.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Klaar.Location = new System.Drawing.Point(12, 380);
+            this.Klaar.Name = "Klaar";
+            this.Klaar.Size = new System.Drawing.Size(250, 134);
+            this.Klaar.TabIndex = 0;
+            this.Klaar.Text = "Klaar";
+            this.Klaar.UseVisualStyleBackColor = true;
+            this.Klaar.Click += new System.EventHandler(this.Klaar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.textBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.textBox1.Location = new System.Drawing.Point(6, 48);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(256, 308);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Item Box";
+            // 
+            // name
+            // 
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(12, 12);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(250, 30);
+            this.name.TabIndex = 2;
+            this.name.Text = "name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 526);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Fris2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.Fris1);
+            this.Controls.Add(this.Bier2);
+            this.Controls.Add(this.Bier1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Klaar);
+            this.Controls.Add(this.name);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Streeplijst";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Klaar;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Button Bier1;
+        private System.Windows.Forms.Button Bier2;
+        private System.Windows.Forms.Button Fris1;
+        private System.Windows.Forms.Button Fris2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        internal System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
