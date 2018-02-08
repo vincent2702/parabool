46a9b1173a00d2df9b1bf114f0f72791622aebd0
vincent2702 <vincent.schelwald@hotmail.com>
2018-02-09 00:24:44 +0100

    asd

---
+++ parabool/parabool/streeplijst/streeplijst_library/Lijst.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace streeplijst_library
+{
+    public class Lijst
+    {
+        public List<Lid> Leden { get; set; }
+        public List<Item> Items { get; set; }
+
+        public Lijst()
+        {
+            Leden = new List<Lid>();
+            Items = new List<Item>();
+        }
+    }
+}
